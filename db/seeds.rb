8e4a351bcf06fc8f32d0068a6137022c4c70d782
Steve Redka <cogitofallacy@gmail.com>
2018-10-01 06:55:35 +0300

    fix: rewrite seeds according to task description

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,27 @@
 # seeds.rb
 
-POSTS_COUNT = 200_000 + Post.count
+POSTS_COUNT = 200_000
+USERS_COUNT = 100
+IPS_COUNT = 50
 
-def create_user_with_posts(posts_count, ip)
-  user = User.create(login: Faker::Name.unique.name)
-  posts_count.times do
-    pst = user.posts.new(header: Faker::Lorem.sentence,
-                            content: Faker::Lorem.paragraph(2),
-                            ip: ip)
+ips = Array.new(IPS_COUNT) { Faker::Internet.unique.public_ip_v4_address }
 
-    pst.save(validate: false)
+logins = Array.new(USERS_COUNT) { Faker::Name.unique.name }
 
-    if (Post.count % 1000).zero?
-      puts "#{Post.count}/#{POSTS_COUNT} posts created"
+puts 'Creating posts'
+POSTS_COUNT.times do |counter|
+  pst = Posts::PostCreateHandler.new(header: Faker::Lorem.sentence,
+                                     content: Faker::Lorem.paragraph(2),
+                                     login: logins.sample,
+                                     ip: ips.sample).call
+
+  if pst.id % 1000 == 3 # Rate only 0.1% of posts
+    rand(0..10).times do
+      Ratings::RateHandler.new(post_id: pst.id,
+                               value: rand(1..5),
+                               silent: true).call
     end
-
-    # Create ratings for only 1% of posts
-    create_some_ratings(pst) if rand(0...100) < 1
   end
-end
 
-def create_some_ratings(pst)
-  rand(0..10).times do
-    pst.ratings.create(value: rand(0..5))
-  end
-end
-
-puts 'Creating normal users with their posts'
-40.times do
-  ip = Faker::Internet.unique.public_ip_v4_address
-  create_user_with_posts(POSTS_COUNT / 100, ip)
-end
-
-puts 'Creating trolls and bots with their posts'
-10.times do
-  ip = Faker::Internet.unique.public_ip_v4_address
-  6.times do
-    create_user_with_posts(POSTS_COUNT / 100, ip)
-  end
+  print "#{counter}/#{POSTS_COUNT}\r" if (counter % 10).zero?
 end
