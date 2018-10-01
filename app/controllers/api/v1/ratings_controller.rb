b260af0bc74c2ea48abd71e7c541b73cf0605aba
Steve Redka <cogitofallacy@gmail.com>
2018-10-01 09:17:45 +0300

    fix: remove debug command

--- app/controllers/api/v1/ratings_controller.rb
+++ app/controllers/api/v1/ratings_controller.rb
@@ -2,7 +2,6 @@ class Api::V1::RatingsController < ApplicationController
   include JSONErrors
 
   def rate
-    p params
     @post = Post.find(rating_params[:post_id])
     Ratings::RateHandler.new(rating_params).call
     render json: { 'post': @post.id, 'new rating': @post.average_rating }
